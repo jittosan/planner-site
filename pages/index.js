ead26e9f5ae8de25493e7bd34776d2c0cd910108
Ratnajit Sarkar <ratnajit.ss@gmail.com>
2022-07-22 14:33:34 +0800

    Implement SASS styling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import styles from '../styles/index.module.scss'
+
 export default function Home() {
     return(
         <div>
-            <h1>Home</h1>
+            <h1 className={styles.title}>Home</h1>
         </div>
     )
 }
