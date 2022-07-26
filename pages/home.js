7371052502d4e205d689eae93c1661ffce5bec01
Ratnajit Sarkar <ratnajit.ss@gmail.com>
2022-07-26 13:46:25 +0800

    styling splash screen

--- pages/home.js
+++ pages/home.js
@@ -1,12 +1,19 @@
+import Head from 'next/head'
 import React from 'react'
+import appConfig from '../config/app-config.json'
 import SplashScreen from '../components/SplashScreen'
 
 const home = () => {
     return (
+        <>
+        <Head>
+            <title>{appConfig.title}</title>
+        </Head>
         <main>
             <SplashScreen />
             <h1>CONTENT</h1>
         </main>
+        </>
     )
 }
 
