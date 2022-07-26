e00a659ef00434ea9d9e991b8487e7e3bd190893
Ratnajit Sarkar <ratnajit.ss@gmail.com>
2022-07-26 12:10:17 +0800

    admin

---
+++ pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SplashScreen from '../components/SplashScreen'
+
+const home = () => {
+    return (
+        <main>
+            <SplashScreen />
+            <h1>CONTENT</h1>
+        </main>
+    )
+}
+
+export default home
