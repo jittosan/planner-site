4f99a207f4fd1470d665f7e0860bf501150c88dd
Ratnajit Sarkar <ratnajit.ss@gmail.com>
2022-07-26 13:59:32 +0800

    finetune splash screen dissolve animation

--- components/SplashScreen.js
+++ components/SplashScreen.js
@@ -3,10 +3,11 @@ import { MdOutlineChromeReaderMode } from 'react-icons/md'
 import React, { useEffect, useRef } from 'react'
 import appConfig from '../config/app-config.json'
 import styles from '../styles/SplashScreen.module.scss'
-import Head from 'next/head'
+
+let selector = (el, tag) => {return gsap.utils.selector(el)(tag)}
 
 const SplashScreen = () => {
-    //splash animation on load
+    //ANIMATION EFFECTS
     let contentRef = useRef()
     let containerRef = useRef()
     useEffect(() => {
@@ -20,37 +21,40 @@ const SplashScreen = () => {
         })
         // splash wave out
         tl.fromTo(containerRef, {
-            height: '100%',
+            height: '100vh',
         }, {
-            height: '0%',
+            height: '10vh',
             duration: 0.8,
             delay: 0.5,
             ease: Power3.easeInOut
         })
         // fade into content
-        tl.fromTo(contentRef, {
-            opacity: 1,
+        tl.fromTo(selector(contentRef, "h1"), {
+            fontSize: '4rem',
         }, {
-            opacity: 0,
+            fontSize: '1.5rem',
             duration: 0.4,
         }, "<25%")
-        
+        tl.fromTo(selector(contentRef, 'p'), {
+            opacity: 1,
+            scale: 1
+        }, {
+            opacity: 0,
+            scale: 0.5,
+            duration: 0.4
+        }, "<25%")
+    
     }, [])
 
 
     return (
-        <>
-        <Head>
-            <title>{appConfig.title}</title>
-        </Head>
         <div className={styles.container} ref={(el) => {containerRef=el}}>
             <div className={styles.content} ref={(el) => {contentRef=el}}>
-                <MdOutlineChromeReaderMode />
+                {/* <MdOutlineChromeReaderMode /> */}
                 <h1>{appConfig.title}</h1>
-                <p>created by ESP</p>
+                {/* <p>by ESP</p> */}
             </div>
         </div>
-        </>
     )
 }
 
